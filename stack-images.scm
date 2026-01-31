fd518ebaf920719b335371130945e81c123c5eb0
Benjamin Krug <benjaminkrug@yahoo.com>
2026-01-31 12:05:19 -0500

    Beginning to update to Gimp 3.0

--- stack-images.scm
+++ stack-images.scm
@@ -1,43 +1,33 @@
-(define (script-fu-move-layer-to image layer absolutex absolutey)
-	(let* ( 
-			(layerOffsets (gimp-drawable-offsets layer))
-			(layerOffsetsX (car layerOffsets))
-			(layerOffsetsY (cadr layerOffsets))
-			(activelayer 0)
-			(offsetx 0)
-			(offsety 0)
-			)
-		(set! activelayer (car (gimp-image-get-active-layer image)))
-		(set! offsetx (- absolutex layerOffsetsX))
-		(set! offsety (- absolutey layerOffsetsY))
-		(gimp-layer-translate activelayer offsetx offsety)
-		(gimp-displays-flush)
-    )
-)
-
 (define (stack-images fnm-L
                       fnm-R)
-   (let* (
-           (img (car (gimp-file-load RUN-NONINTERACTIVE fnm-L fnm-L)))
-           (layL (car (gimp-image-get-active-layer img)))
-           (layR (car (gimp-file-load-layer RUN-NONINTERACTIVE img fnm-R)))
-		   (origHeight (car (gimp-image-height img)))
-		   (origWidth (car (gimp-image-width img)))
+   (let* 
+      (
+         (img (car (gimp-file-load RUN-NONINTERACTIVE fnm-L fnm-L)))
+         (layL (car (gimp-image-get-layers img)))
+         (layR (car (gimp-file-load-layer RUN-NONINTERACTIVE img fnm-R)))
+		   (origHeight (car (gimp-image-get-height img)))
+		   (origWidth (car (gimp-image-get-width img)))
 		   (newHeight (* 2 origHeight))
 		   (CLIP_TO_IMAGE 1)
-         )
-	  ; double image height
-	  (gimp-image-resize img origWidth newHeight 0 0)
+      )
+	   ; double image height
+	   (gimp-image-resize img origWidth newHeight 0 0)
       ; insert the right-eye layer
-      (gimp-image-add-layer img layR 0)
-	  ; move right layer vertically down
-	  (script-fu-move-layer-to img layR 0 origHeight)
-	  ; merge layers
-	  (gimp-image-merge-visible-layers img CLIP_TO_IMAGE)
-	  ; display the image
-	  (gimp-display-new img)
-      ; the end
-      (gimp-displays-flush)
+      (gimp-image-insert-layer img layR 0 0)
+	   ; move right layer vertically down
+	   (gimp-layer-set-offsets layR 0 origHeight)
+	   ; merge layers
+	   (let*
+         (
+            (layMerge (car (gimp-image-merge-visible-layers img CLIP_TO_IMAGE) ) )
+         )
+         ; display the image
+         (gimp-display-new img)
+         ; the end
+         (gimp-displays-flush)
+         ; return
+         '(img layMerge)         
+      )
    )
 )
 
@@ -53,4 +43,6 @@
                     SF-FILENAME "fnm-R" ""
                     ;SF-FILENAME "Output" "output.rcs.png"
 )
-(script-fu-menu-register "stack-images" "<Toolbox>/Stereo")
+
+(script-fu-menu-register "stack-images"
+						 "<Image>/Stereo")
--- stereo-photo.scm
+++ stereo-photo.scm
@@ -1,11 +1,9 @@
 (define (stereo-photo img lyr backColor borderSize)
    (let* (
-		   (origHeight (car (gimp-image-height img)))
-		   (origWidth (car (gimp-image-width img)))
+		   (origHeight (car (gimp-image-get-height img)))
+		   (origWidth (car (gimp-image-get-width img)))
 		   (newHeight (/ origHeight 2))
 		   (newWidth (* 2 origWidth))
-		   (CHANNEL_OP_ADD 0)
-		   (CHANNEL_OP_REPLACE 2)
 		   (CLIP_TO_IMAGE 1)
 		   (FG_BUCKET_FILL 0)
 		   (BACKGROUND_FILL 1)
@@ -16,18 +14,18 @@
 	  (gimp-image-resize img newWidth origHeight 0 0)
 	  (gimp-layer-resize-to-image-size lyr)
       ; select bottom half of image
-      (gimp-image-select-rectangle img CHANNEL_OP_ADD 0 newHeight origWidth newHeight)
+      (gimp-image-select-rectangle img CHANNEL-OP-ADD 0 newHeight origWidth newHeight)
 	  ; cut bottom half of image
-	  (gimp-edit-cut lyr)
+	  (gimp-edit-cut (vector lyr) )
       ; select top right quarter of new image size
-      (gimp-image-select-rectangle img CHANNEL_OP_REPLACE origWidth 0 origWidth newHeight)
+      (gimp-image-select-rectangle img CHANNEL-OP-REPLACE origWidth 0 origWidth newHeight)
 	  ; paste bottom half on the right
-	  (gimp-edit-paste lyr 1)
+	  (gimp-edit-paste lyr)
 	  ; merge layers
 	  (gimp-image-merge-visible-layers img CLIP_TO_IMAGE)
 	  ; halve image height
 	  (gimp-image-resize img newWidth newHeight 0 0)
-	  (gimp-layer-resize-to-image-size (car (gimp-image-get-active-layer img)))
+	  (gimp-layer-resize-to-image-size (car (gimp-image-get-layers img)))
 	  ; set image to ration of a 4x6 photograph
 	  (let* (
 				(heightWithBorder (/ newHeight (- 1 (/ borderSize (/ HEIGHT_INCHES 2)))))
@@ -40,7 +38,7 @@
 				(let* (
 						(centerHeight (/ (- photoHeight newHeight) 2))
 						(centerWidth (/ (- photoWidth newWidth) 2))
-						(lyr (car (gimp-image-get-active-layer img)))
+						(lyr (car (gimp-image-get-layers img)))
 						(blankLyr (car (gimp-layer-new img photoWidth photoHeight 0 "" 100 0)))
 					)
 					(gimp-image-resize img photoWidth photoHeight 0 0)
@@ -74,7 +72,7 @@
 					SF-IMAGE      "The image"				0
 					SF-DRAWABLE   "The layer"				0
 					SF-COLOR      _"Color"                  "gray"
-					SF-ADJUSTMENT _"Border Size in Inches" '(0.0625 0 3 0.0625 5 4 0)
+					SF-ADJUSTMENT _"Border Size in Inches"  '(0.0625 0 3 0.0625 5 4 0)
 )
 
 (script-fu-menu-register "stereo-photo"
