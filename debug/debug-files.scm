55f44780cce3f5e231b6f066a288767f27ba1e54
Benjamin Krug <benjaminkrug@yahoo.com>
2026-02-14 16:52:26 -0500

    Change debug script

--- debug/debug-files.scm
+++
@@ -1,3 +0,0 @@
-
-
-;(analygraph-mass-creation "/home/bkrug/Pictures/Photos/Switch 2 Unboxing/3D/Left" "/home/bkrug/Pictures/Photos/Switch 2 Unboxing/3D/Right" "/home/bkrug/Pictures/Photos/Switch 2 Unboxing/3D/Ana" "jpg")
---
+++ debug/debug-mass-analygraph.scm
@@ -0,0 +1,1 @@
+(analygraph-mass-creation "/home/bkrug/Pictures/Photos/Switch 2 Unboxing/3D/Left" "/home/bkrug/Pictures/Photos/Switch 2 Unboxing/3D/Right" "/home/bkrug/Pictures/Photos/Switch 2 Unboxing/3D/Ana" "jpg")
