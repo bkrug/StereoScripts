ae3cf0f908fd345fc391b9b273672fb1c04ce7d1
Benjamin Krug <benjaminkrug@yahoo.com>
2026-02-14 16:51:41 -0500

    Move mass creation methods to the longer term script folder

--- src/analygraph-layers.scm
+++ src/analygraph-layers.scm
@@ -28,9 +28,72 @@
 	)
 )
 
+(define (normalize-folder-path givenPath)
+	(if
+		(equal?
+			(substring givenPath (- (string-length givenPath) 1))
+			"/"
+		)
+		givenPath
+		(string-append givenPath "/")
+	)
+)
+
+(define (normalize-extension ext)
+  (cond
+    ((equal? (substring ext 0 2) "*.")
+      givenExt)
+    ((equal? (substring ext 0 1) ".")
+      (string-append "*" ext))
+    (else
+      (string-append "*." ext))
+  )
+)
+
+; "folderL", "folderR", and "destFolder" must end with slashes
+(define (analygraph-save-image fnmL folderL folderR destFolder)
+	(let*
+		(
+			(filename (substring fnmL (string-length folderL)))
+			(fnmR (string-append folderR filename))
+			(destFile (string-append destFolder filename))
+			(img (analygraph-create-layers fnmL fnmR))
+		)
+		(gimp-image-flatten img)
+		(gimp-file-save RUN-NONINTERACTIVE img destFile)
+	)
+)
+
+(define (analygraph-save-many-images filesL folderL folderR dest)
+	(let*
+		(
+			(errorMsg (analygraph-save-image (car filesL) folderL folderR dest))
+		) 
+		(if (> (length filesL) 1)
+			(analygraph-save-many-images (cdr filesL) folderL folderR dest)
+			(display "")
+		)
+	)
+)
+
+(define (analygraph-mass-creation pathL pathR pathDest ext)
+	(let*
+		(
+			(normalizedL (normalize-folder-path pathL))
+			(normalizedR (normalize-folder-path pathR))
+			(normalizedDest (normalize-folder-path pathDest))
+			(normalizedExt (normalize-extension ext))
+			(searchL (string-append normalizedL normalizedExt))
+			(filesL (car (file-glob searchL 0)))
+		)
+    (gimp-message searchL)
+		(analygraph-save-many-images filesL normalizedL normalizedR normalizedDest)
+	)
+)
+
 (script-fu-register "analygraph-display-layers"
 					"Load Analygraph Layers"
-					"Loads two photos as layers tinted cyan and red"
+					"Loads two photos as separate layers tinted cyan and red"
 					""
 					"Benjamin Krug"
 					"2026-02-14"
@@ -39,5 +102,21 @@
           SF-FILENAME "Right Image" ""
 )
 
+(script-fu-register "analygraph-mass-creation"
+					"Create Analygraph Images in Mass"
+					"Specify two folders with matching lists of left and right images, and place new analygraphs in a third folder"
+					""
+					"Benjamin Krug"
+					"2026-02-14"
+					""
+          SF-DIRNAME "Left Folder" ""
+          SF-DIRNAME "Right Folder" ""
+          SF-DIRNAME "Destination Folder" ""
+          SF-STRING "Extension" ""
+)
+
 (script-fu-menu-register "analygraph-display-layers"
                          "<Image>/Stereo")
+
+(script-fu-menu-register "analygraph-mass-creation"
+                         "<Image>/Stereo")                         
