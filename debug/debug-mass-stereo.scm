97dcaa69c038aa8fd95bc9fb537fd90c637ed19e
Benjamin Krug <benjaminkrug@yahoo.com>
2026-02-15 02:03:27 -0500

    Add a border

--- debug/debug-mass-stereo.scm
+++ debug/debug-mass-stereo.scm
@@ -1,3 +1,59 @@
+(define (add-border img
+					backColor
+					borderSize)
+	; set image to ratio of a 4x6 photograph
+	(let* 
+		(
+			(layerArray (car (gimp-image-get-layers img)))
+			(lyrContent (vector-ref layerArray 0))
+			;			
+			(contentHeight (car (gimp-image-get-height img)))
+			(contentWidth (car (gimp-image-get-width img)))         
+			(HEIGHT_INCHES 4)
+			(WIDTH_INCHES 6)         
+			; height and width in pixels before they are increased to match the desired aspect ratio
+			(heightWithBorder (/ contentHeight (- 1 (/ (* 2 borderSize) HEIGHT_INCHES ))))
+			(widthWithBorder (/ contentWidth (- 1 (/ (* 2 borderSize) WIDTH_INCHES ))))
+			; height and width in pixels after increasing them to match the desired aspect ratio
+			(printedHeight
+				(if
+					(> (/ HEIGHT_INCHES WIDTH_INCHES) (/ contentHeight contentWidth))
+					(* (/ HEIGHT_INCHES WIDTH_INCHES) widthWithBorder)
+					heightWithBorder 
+				)
+			)
+			(printedWidth
+				(if
+					(> (/ HEIGHT_INCHES WIDTH_INCHES) (/ contentHeight contentWidth))
+					widthWithBorder
+					(* (/ WIDTH_INCHES HEIGHT_INCHES) heightWithBorder)
+				)
+			)
+			; offsets to move the content to
+			(contentTopOffset (/ (- printedHeight contentHeight) 2))
+			(contentLeftOffset (/ (- printedWidth contentWidth) 2))
+			; calculate the desired dots per inch
+			(verticalResolution (/ printedHeight HEIGHT_INCHES))
+			(horizontalResolution (/ printedWidth WIDTH_INCHES))
+			; Create a background layer to hold the border color
+			(blankLyr (car (gimp-layer-new img "Background" printedWidth printedHeight 0 100 0)))
+		)
+		;
+		(gimp-image-resize img printedWidth printedHeight 0 0)
+		; center the content layer vertically and horizontally
+		(gimp-layer-set-offsets lyrContent contentLeftOffset contentTopOffset)
+		; color background image
+		(gimp-context-set-background backColor)
+		(gimp-drawable-fill blankLyr FILL-BACKGROUND)
+		; insert layer behind
+		(gimp-image-insert-layer img blankLyr 0 1)
+		; merge layers
+		(gimp-image-merge-visible-layers img CLIP-TO-IMAGE)
+		; set resolution so that the image makes sense in inches
+		(gimp-image-set-resolution img horizontalResolution verticalResolution)
+	)
+)
+
 (define (stereo-auto fnmL
                      fnmR)
 	(let* 
@@ -21,16 +77,14 @@
 		; cut right layer down to middle portion and move it to the right
 		(gimp-layer-resize lyrR widthForEachEye origHeight cutFromLeftWidth 0)
 		(gimp-layer-set-offsets lyrR widthForEachEye 0)
-		;
+		; resize image to a 4x6 ratio
 		(gimp-image-resize img newWidth origHeight 0 0)
-		(let*
-			(
-				(layMerge (car (gimp-image-merge-visible-layers img CLIP-TO-IMAGE) ) )
-			)
-			; display the image
-			(gimp-display-new img)
-			(gimp-displays-flush)
-		)		
+		(gimp-image-merge-visible-layers img CLIP-TO-IMAGE)
+		;
+		(add-border img "gray" 0.0625)
+		; display the image
+		(gimp-display-new img)
+		(gimp-displays-flush)
 	)
 )
 
